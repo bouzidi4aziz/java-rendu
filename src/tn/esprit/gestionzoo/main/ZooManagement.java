66604b5ca12680eb0c661275b0ee19d410e7e016
aziz bouzidi <aziz.bouzidi@esprit.tn>
2023-10-15 14:37:09 +0200

    prosit5

--- src/tn/esprit/gestionzoo/main/ZooManagement.java
+++ src/tn/esprit/gestionzoo/main/ZooManagement.java
@@ -1,7 +1,6 @@
 package tn.esprit.gestionzoo.main;
 
-import tn.esprit.gestionzoo.entities.Animal;
-import tn.esprit.gestionzoo.entities.Zoo;
+import tn.esprit.gestionzoo.entities.*;
 
 public class ZooManagement {
 
@@ -44,6 +43,20 @@ public class ZooManagement {
 //        System.out.println(tn.esprit.gestionzoo.entities.Zoo.comparerZoo(myZoo, notMyZoo));
 //        System.out.println(myZoo.isZooFull());
 
+        //Prosit5
+        Aquatic Aquatic1 = new Aquatic("h","aziz",22,true,"beja");
+        Terrestrial terrestrial1 = new Terrestrial("z","jacem",23,true,4);
+        Dolphin Dolphin1 = new Dolphin("d","dali",24,false,"beja",2);
+        Penguin Penquin1 = new Penguin("e","omar",25,false,"beja",1);
+
+        System.out.println(Aquatic1.toString());
+        System.out.println(terrestrial1.toString());
+        System.out.println(Dolphin1.toString());
+        System.out.println(Penquin1.toString());
+
+        Dolphin1.swim();
+        Aquatic1.swim();
+        Penquin1.swim();
     }
 
 }
